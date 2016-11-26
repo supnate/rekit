e17b639accbd02e799764cacef47b56b416e0d1d
Nate Wang <supnate@gmail.com>
2016-11-27 00:32:34 +0800

    Add profiling flag for build.

--- tools/build.js
+++ tools/build.js
@@ -8,6 +8,42 @@ const crypto = require('crypto');
 const helpers = require('./lib/helpers');
 const webpack = require('webpack');
 const config = require('../webpack-config')('dist');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ArgumentParser = require('argparse').ArgumentParser;
+
+const parser = new ArgumentParser({
+  addHelp: true,
+  description: 'Whether to show bundle content as convenient interactive zoomable treemap',
+});
+
+parser.addArgument(['-p', '--profile'], {
+  help: 'Whether to show profile of the bundle.',
+  action: 'storeTrue',
+});
+
+const args = parser.parseArgs();
+
+// Show profile of the build bundle
+if (args.profile) {
+  config.plugins.push(new BundleAnalyzerPlugin({
+    // Can be `server`, `static` or `disabled`.
+    // In `server` mode analyzer will start HTTP server to show bundle report.
+    // In `static` mode single HTML file with bundle report will be generated.
+    // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+    analyzerMode: 'static',
+    // Path to bundle report file that will be generated in `static` mode.
+    // Relative to bundles output directory.
+    reportFilename: 'report.html',
+    // Automatically open report in default browser
+    openAnalyzer: true,
+    // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+    generateStatsFile: false,
+    // Options for `stats.toJson()` method.
+    // For example you can exclude sources of your modules from stats file with `source: false` option.
+    // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+    statsOptions: null
+  }));
+}
 
 // Clean folder
 const buildFolder = path.join(__dirname, '../build');

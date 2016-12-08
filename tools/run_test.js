1db4436da6b2dbc76897a8b5da13bc474b7de233
Nate Wang <supnate@gmail.com>
2016-12-09 00:49:24 +0800

    Fix tests.

--- tools/run_test.js
+++ tools/run_test.js
@@ -9,24 +9,20 @@
 
 'use strict';
 const path = require('path');
-const shell = require('shelljs');
+// const shell = require('shelljs');
 const npmRun = require('npm-run');
 
 const prjRoot = path.join(__dirname, '../');
 
-let args = process.argv[2];
-let testFile = null;
-if (!args) {
-  args = 'app';
+let testFile = process.argv[2];
+let needReport = false;
+if (!testFile) {
+  needReport = true;
+  testFile = path.join(prjRoot, 'tests/**/*.test.js');
+} else {
+  testFile = path.join(prjRoot, 'tests', testFile);
 }
-
-if (args !== 'all') {
-  testFile = path.join(prjRoot, 'tests', args);
-  if (shell.test('-d', testFile)) {
-    testFile = path.join(testFile, '**/*.test.js');
-  }
-}
-console.log(`Running test${(!testFile || /\*/.test(testFile)) ? 's' : ''}: `, (testFile || 'all').replace(prjRoot, ''), '...');
+console.log('Running tests: ', testFile.replace(prjRoot, ''), '...');
 
 const env = Object.create(process.env);
 env.NODE_ENV = 'test';
@@ -36,68 +32,19 @@ const opts = {
   env,
 };
 
-const needReport = args === 'all' || args === 'app' || args === 'cli';
+const params = [
+  'mocha-webpack',
+  '--include',
+  'tests/before-all.js',
+  '--webpack-config',
+  'webpack.test.config.js',
+  `"${testFile || path.join(prjRoot, 'tests/**/*.test.js')}"`,
+];
 
-function runAppTest() {
-  const params = [
-    'mocha-webpack',
-    '--include',
-    'test/app/before-all.js',
-    '--webpack-config',
-    'webpack.test.config.js',
-    `"${testFile || path.join(prjRoot, 'test/app/**/*.test.js')}"`,
-  ];
-
-  if (needReport) {
-    params.splice(0, 0,
-      'nyc',
-      '--report-dir=coverage/app'
-    );
-  }
-  npmRun.execSync(params.join(' '), opts);
+if (needReport) {
+  params.splice(0, 0,
+    'nyc',
+    '--report-dir=coverage'
+  );
 }
-
-function runCliTest() {
-  const params = [
-    'mocha',
-    testFile || path.join(prjRoot, 'test/cli/**/*.test.js'),
-  ];
-
-  if (needReport) {
-    params.splice(0, 0,
-      'nyc',
-      '--report-dir=coverage/cli'
-    );
-  }
-  npmRun.execSync(params.join(' '), opts);
-}
-
-function runAllTest() {
-  const coverageFolder = path.join(prjRoot, 'coverage');
-  if (!shell.test('-e', coverageFolder)) {
-    shell.mkdir(coverageFolder);
-  }
-  const cacheFolder = path.join(coverageFolder, '.nyc_output');
-  if (shell.test('-e', cacheFolder)) {
-    shell.rm('-rf', cacheFolder);
-  }
-  shell.mkdir(cacheFolder);
-  runAppTest();
-  shell.cp('-R', path.join(prjRoot, '.nyc_output/*'), cacheFolder);
-  runCliTest();
-  shell.cp('-R', `${cacheFolder}/*`, path.join(prjRoot, '.nyc_output'));
-  npmRun.execSync('nyc report --reporter=text-summary --reporter=lcov', opts);
-  console.log('Overall coverage report: ', path.join(prjRoot, 'coverage/lcov-report/index.html'));
-  shell.rm('-rf', cacheFolder);
-}
-
-if (/^app/.test(args)) {
-  runAppTest();
-} else if (/^cli/.test(args)) {
-  runCliTest();
-} else if (/^all/.test(args)) {
-  runAllTest();
-} else {
-  console.error('Test files should be under test/app or test/cli.');
-  process.exit(1);
-}
+npmRun.execSync(params.join(' '), opts);

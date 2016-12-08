1080c9f5b28e07e0d126327e06f870c588b848db
Nate Wang <supnate@gmail.com>
2016-12-08 19:30:06 +0800

    Using rekit-core

--- bin/rekit.js
+++ bin/rekit.js
@@ -6,8 +6,34 @@
 // Usage:
 //  rekit action [...args]
 
+const path = require('path');
+const shell = require('shelljs');
 const ArgumentParser = require('argparse').ArgumentParser;
 const rekitPkgJson = require('../package.json');
+const create = require('./create');
+
+// If runs under a project
+function getLocalRekitCore() {
+  let cwd = process.cwd();
+  let lastDir = null;
+  let prjRoot = null;
+  // Traverse above until find the package.json.
+  while (cwd && lastDir !== cwd) {
+    if (shell.test('-e', path.join(cwd, 'package.json'))) {
+      prjRoot = cwd;
+      break;
+    }
+    lastDir = cwd;
+    cwd = path.join(cwd, '..');
+  }
+
+  if (!prjRoot || !shell.test('-e', path.join(prjRoot, 'node_modules/rekit-core/package.json'))) {
+    return null;
+  }
+  return require(path.join(prjRoot, 'node_modules/rekit-core'));
+}
+
+const rekitCore = getLocalRekitCore();
 
 const parser = new ArgumentParser({
   version: rekitPkgJson.version,
@@ -15,11 +41,105 @@ const parser = new ArgumentParser({
   description: 'Build scalable web applications with React, Redux and React-router.'
 });
 
-parser.addArgument('action', {
-  help: 'Name of the app.',
+// parser.addArgument('action', {
+//   help: 'The action to take. Rekit core provides [add, remove, move]',
+// });
+
+const subparsers = parser.addSubparsers({
+  title: 'Sub commands',
+  dest: 'subCommandName',
 });
+
+// Create project
+const createCmd = subparsers.addParser('create',
+  {
+    addHelp: true,
+    description: 'Create a new Rekit project.',
+  }
+);
+
+createCmd.addArgument('name', {
+  help: 'The project name',
+});
+
+createCmd.addArgument(['--plugin', '-p'], {
+  help: 'Indicate its a Rekit plugin project',
+  action: 'storeTrue',
+});
+
+// Add sub-command
+const addCmd = subparsers.addParser('add',
+  {
+    addHelp: true,
+    description: 'Add an element to the project.',
+  }
+);
+
+addCmd.addArgument('type', {
+  help: 'The type of the element to add.'
+});
+
+addCmd.addArgument('name', {
+  help: 'The element name to add, in format of <feature>/<name>, e.g.: \'rekit add component user/list-view\'. Name is unnecessary if add a feature.'
+});
+
+// Remove sub-command
+const rmCmd = subparsers.addParser('remove',
+  {
+    aliases: ['rm'],
+    addHelp: true,
+    description: 'Remove an element from the project.',
+  }
+);
+
+rmCmd.addArgument('type', {
+  help: 'The type of the element to remove.'
+});
+
+rmCmd.addArgument('name', {
+  help: 'The element name to remove, in format of <feature>/<name>, e.g.: \'rekit remove component user/list-view\'. Name is unnecessary if remove a feature.'
+});
+
+// Move sub-command
+const mvCmd = subparsers.addParser('move',
+  {
+    aliases: ['mv'],
+    addHelp: true,
+    description: 'Move or rename an element.',
+  }
+);
+
+mvCmd.addArgument('type', {
+  help: 'The type of the element to move.'
+});
+
+mvCmd.addArgument('source', {
+  help: 'The source element to move, in format of <feature>/<name>, e.g.: \'rekit move component user/list-view employee/list\'. Name is unnecessary if move a feature.'
+});
+
+mvCmd.addArgument('target', {
+  help: 'The destination element to reach, in format of <feature>/<name>, e.g.: \'rekit move component user/list-view employee/list\'. Name is unnecessary if move a feature.'
+});
+
+// Load command supports from plugins
 
 const args = parser.parseArgs();
 
-console.log(args);
 
+switch (args.subCommandName) {
+  case 'create':
+    create(args);
+    break;
+  case 'add':
+    // rekitCore.addComponnet(args);
+    break;
+  case 'remove':
+    break;
+  case 'move':
+    break;
+  default:
+    rekitCore.handleCommand(args);
+    break;
+}
+
+// vio.flush();

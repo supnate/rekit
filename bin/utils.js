5c9853b9bf5de8fb249451cbf8f511f9679beb8d
Nate Wang <supnate@gmail.com>
2016-12-22 19:25:25 +0800

    Use request lib so that it respects proxy.

--- bin/utils.js
+++ bin/utils.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
+const request = require('request'); // use request lib so that it respects proxy
 
 // Credit to: http://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js
 function copyFileSync(source, target) {
@@ -58,39 +59,48 @@ function deleteFolderRecursive(dirPath) {
 
 function requestDeps() {
   return new Promise((resolve, reject) => {
-    https.get('https://supnate.github.io/rekit-deps/deps.2.x.json', (res) => {
-      const statusCode = res.statusCode;
-      const contentType = res.headers['content-type'];
-
-      let error;
-      if (statusCode !== 200) {
-        error = new Error(`Request Failed.\n` +
-                          `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error(`Invalid content-type.\n` +
-                          `Expected application/json but received ${contentType}`);
+    request('https://supnate.github.io/rekit-deps/deps.2.x.json', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // console.log(body) // Show the HTML for the Google homepage. 
+        resolve(JSON.parse(body));
+      } else {
+        reject(error || response.statusCode);
       }
-      if (error) {
-        reject(error.message);
-        // consume response data to free up memory
-        res.resume();
-        return;
-      }
+    })
 
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          resolve(parsedData);
-        } catch (e) {
-          reject(e.message);
-        }
-      });
-    }).on('error', (e) => {
-      reject(e.message);
-    });
+    // https.get('https://supnate.github.io/rekit-deps/deps.2.x.json', (res) => {
+    //   const statusCode = res.statusCode;
+    //   const contentType = res.headers['content-type'];
+
+    //   let error;
+    //   if (statusCode !== 200) {
+    //     error = new Error(`Request Failed.\n` +
+    //                       `Status Code: ${statusCode}`);
+    //   } else if (!/^application\/json/.test(contentType)) {
+    //     error = new Error(`Invalid content-type.\n` +
+    //                       `Expected application/json but received ${contentType}`);
+    //   }
+    //   if (error) {
+    //     reject(error.message);
+    //     // consume response data to free up memory
+    //     res.resume();
+    //     return;
+    //   }
+
+    //   res.setEncoding('utf8');
+    //   let rawData = '';
+    //   res.on('data', (chunk) => { rawData += chunk; });
+    //   res.on('end', () => {
+    //     try {
+    //       const parsedData = JSON.parse(rawData);
+    //       resolve(parsedData);
+    //     } catch (e) {
+    //       reject(e.message);
+    //     }
+    //   });
+    // }).on('error', (e) => {
+    //   reject(e.message);
+    // });
   });
 }
 

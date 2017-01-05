5bd7ecf06bfd15b415608150f1dc3b3fb159972e
Nate Wang <supnate@gmail.com>
2017-01-05 22:49:49 +0800

    update test.js

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const core = require('rekit-core');
-console.time('abc');
-console.log(core.refactor.getFeatureStructure('home').misc);
-console.timeEnd('abc');
+
+const refactor = core.refactor;
+const vio = core.vio;
+const utils = core.utils;
+
+utils.setProjectRoot('/Users/i305656/workspace/rekit-portal');
+
+console.time('timer');
+const file = utils.mapFeatureFile('home', 'Navigator.js');
+const deps = refactor.getDeps(file);
+console.log(file, deps);
+console.timeEnd('timer');

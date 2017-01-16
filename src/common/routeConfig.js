135a491301de8a3aa0ecda48d07100bc98ea5c00
Nate Wang <supnate@gmail.com>
2017-01-16 19:06:35 +0800

    code refactor

--- src/common/routeConfig.js
+++ src/common/routeConfig.js
@@ -3,14 +3,20 @@ import { PageNotFound } from '../features/common';
 import homeRoute from '../features/home/route';
 import commonRoute from '../features/common/route';
 
-const routes = [{// REKIT_ARCHOR_DO_NOT_CHANGE
+// NOTE: DO NOT CHANGE the 'childRoutes' name and the declaration pattern.
+// This is used for Rekit cmds to register routes config for new features, and remove config when remove features, etc.
+const childRoutes = [
+  homeRoute,
+  commonRoute,
+];
+
+const routes = [{
   path: '/',
   component: App,
   childRoutes: [
-    homeRoute,
-    commonRoute,
+    ...childRoutes,
     { path: '*', name: 'Page not found', component: PageNotFound },
-  ].filter(r => r.component || (r.childRoutes && r.childRoutes.length > 0)), // REKIT_ARCHOR_DO_NOT_CHANGE // If a feature has no router rules, remove it.
+  ].filter(r => r.component || (r.childRoutes && r.childRoutes.length > 0)),
 }];
 
 // Handle isIndex property of route config:

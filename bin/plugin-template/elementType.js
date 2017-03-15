9c80f929aee3886a919c481793dc98a92afbb8c5
Nate Wang <supnate@gmail.com>
2017-03-15 19:04:02 +0800

    Refine plugin template.

--- bin/plugin-template/elementType.js
+++ bin/plugin-template/elementType.js
@@ -3,7 +3,7 @@
 // This is the main module of the plugin where you define
 // add, remove, move method to manage elements.
 
-// rekitCore is the one that dependent by the project using the plugin.
+// rekitCore is the one that is dependent by the project using the plugin.
 // You may need it to perform common tasks such as use refactor to rename variables in a module.
 // const rekitCore = require('rekit-core');
 
@@ -19,20 +19,21 @@ function move(source, target) {
   console.log('Moving ${pluginName}.');
 }
 
-function install() {
-  // Called when using 'rekit install plugin-name' to install the plugin.
-  // Should check repeated installation.
-  console.log('Installing ${pluginName}');
-}
-function uninstall() {
-  // Called when using 'rekit uninstall plugin-name' to install the plugin.
-  // Should check repeated uninstallation.
-  console.log('Uninstalling ${pluginName}');
-}
+/* Uncomment below code to enable installation and uninstallation*/
+// function install() {
+//   // Called when using 'rekit install plugin-name' to install the plugin.
+//   // Should check repeated installation.
+//   console.log('Installing ${pluginName}');
+// }
+// function uninstall() {
+//   // Called when using 'rekit uninstall plugin-name' to install the plugin.
+//   // Should check repeated uninstallation.
+//   console.log('Uninstalling ${pluginName}');
+// }
 
 module.exports = {
-  install,
-  uninstall,
+  // install,
+  // uninstall,
   add,
   remove,
   move,

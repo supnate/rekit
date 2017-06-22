9f01286a18f6503f63c490655f2fb27cf7c213bc
Nate Wang <supnate@gmail.com>
2017-06-22 17:35:33 +0800

    Code clean

--- tests/sanity/twoPlugins.js
+++ tests/sanity/twoPlugins.js
@@ -24,66 +24,73 @@ function reset() {
   console.log('Reset environment...');
   _.pull(pkg.rekit.plugins, 'redux-saga');
   _.pull(pkg.rekit.plugins, 'selector');
-  shell.ShellString(JSON.stringify(pkg, null, '  ')).to(appPkgJsonPath);
+  // shell.ShellString(JSON.stringify(pkg, null, '  ')).to(appPkgJsonPath);
 
-  shell.exec('rekit rm action home/a1', { cwd: appRoot });
-  shell.exec('rekit rm action home/a2', { cwd: appRoot });
-  shell.exec('rekit rm action common/a1', { cwd: appRoot });
-  shell.exec('rekit rm action common/a2', { cwd: appRoot });
+  // shell.exec('rekit rm action home/a1', { cwd: appRoot });
+  // shell.exec('rekit rm action home/a2', { cwd: appRoot });
+  // shell.exec('rekit rm action common/a1', { cwd: appRoot });
+  // shell.exec('rekit rm action common/a2', { cwd: appRoot });
   shell.exec('rekit rm selector home/s1', { cwd: appRoot });
-  shell.exec('rekit rm selector home/s2', { cwd: appRoot });
-  shell.exec('rekit rm selector common/s1', { cwd: appRoot });
+  // shell.exec('rekit rm selector home/s2', { cwd: appRoot });
+  // shell.exec('rekit rm selector common/s1', { cwd: appRoot });
   shell.exec('rekit rm selector common/s2', { cwd: appRoot });
 
-  shell.rm('-rf', path.join(appRoot, 'node_modules/rekit-plugin-redux-saga'));
-  shell.rm('-rf', path.join(appRoot, 'node_modules/rekit-plugin-selector'));
+  // shell.rm('-rf', path.join(appRoot, 'node_modules/rekit-plugin-redux-saga'));
+  // shell.rm('-rf', path.join(appRoot, 'node_modules/rekit-plugin-selector'));
+  shell.rm('-rf', path.join(appRoot, 'src/features/common/selectors'));
+  shell.rm('-rf', path.join(appRoot, 'src/features/home/selectors'));
 }
 
 reset();
 
 let pluginRoot;
 
+exec('rekit add selector home/s1', { cwd: appRoot });
+exec('rekit mv selector home/s1 common/s2', { cwd: appRoot });
+expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s1.js'))).to.be.false;
+
+
 // rekit-plugin-redux-saga
-pluginRoot = path.join(appRoot, '../rekit-plugin-redux-saga');
-exec(`npm install ${pluginRoot}`, { cwd: appRoot });
-pkg.rekit.plugins.push('redux-saga');
-shell.ShellString(JSON.stringify(pkg, null, '  ')).to(appPkgJsonPath);
-exec('rekit add action home/a1 -a', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a1.js'))).to.be.true;
-exec('npm test features/home/redux/a1.test.js', { cwd: appRoot });
+// pluginRoot = path.join(appRoot, '../rekit-plugin-redux-saga');
+// exec(`npm install ${pluginRoot}`, { cwd: appRoot });
+// pkg.rekit.plugins.push('redux-saga');
+// shell.ShellString(JSON.stringify(pkg, null, '  ')).to(appPkgJsonPath);
+// exec('rekit add action home/a1 -a', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a1.js'))).to.be.true;
+// exec('npm test features/home/redux/a1.test.js', { cwd: appRoot });
 
-exec('rekit mv action home/a1 home/a2', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a1.js'))).to.be.false;
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a2.js'))).to.be.true;
-exec('npm test features/home/redux/a2.test.js', { cwd: appRoot });
+// exec('rekit mv action home/a1 home/a2', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a1.js'))).to.be.false;
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a2.js'))).to.be.true;
+// exec('npm test features/home/redux/a2.test.js', { cwd: appRoot });
 
-exec('rekit mv action home/a2 common/a1', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a2.js'))).to.be.false;
-expect(shell.test('-e', path.join(appRoot, 'src/features/common/redux/a1.js'))).to.be.true;
-exec('npm test features/common/redux/a1.test.js', { cwd: appRoot });
+// exec('rekit mv action home/a2 common/a1', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/redux/a2.js'))).to.be.false;
+// expect(shell.test('-e', path.join(appRoot, 'src/features/common/redux/a1.js'))).to.be.true;
+// exec('npm test features/common/redux/a1.test.js', { cwd: appRoot });
 
-exec('rekit rm action common/a1', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/common/redux/a1.js'))).to.be.false;
+// exec('rekit rm action common/a1', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/common/redux/a1.js'))).to.be.false;
 
 // rekit-plugin-selector
-pluginRoot = path.join(appRoot, '../rekit-plugin-selector');
-exec(`npm install ${pluginRoot}`, { cwd: appRoot });
-pkg.rekit.plugins.push('selector');
-shell.ShellString(JSON.stringify(pkg, null, '  ')).to(appPkgJsonPath);
-exec('rekit add selector home/s1', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s1.js'))).to.be.true;
-exec('npm test features/home/selectors/s1.test.js', { cwd: appRoot });
+// pluginRoot = path.join(appRoot, '../rekit-plugin-selector');
+// exec(`npm install ${pluginRoot}`, { cwd: appRoot });
+// pkg.rekit.plugins.push('selector');
+// shell.ShellString(JSON.stringify(pkg, null, '  ')).to(appPkgJsonPath);
+// exec('rekit add selector home/s1', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s1.js'))).to.be.true;
+// exec('npm test features/home/selectors/s1.test.js', { cwd: appRoot });
 
-exec('rekit mv selector home/s1 home/s2', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s1.js'))).to.be.false;
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s2.js'))).to.be.true;
-exec('npm test features/home/selectors/s2.test.js', { cwd: appRoot });
+// exec('rekit mv selector home/s1 home/s2', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s1.js'))).to.be.false;
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s2.js'))).to.be.true;
+// exec('npm test features/home/selectors/s2.test.js', { cwd: appRoot });
 
-exec('rekit mv selector home/s2 common/s1', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s2.js'))).to.be.false;
-expect(shell.test('-e', path.join(appRoot, 'src/features/common/selectors/s1.js'))).to.be.true;
-exec('npm test features/common/selectors/s1.test.js', { cwd: appRoot });
+// exec('rekit mv selector home/s2 common/s1', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/home/selectors/s2.js'))).to.be.false;
+// expect(shell.test('-e', path.join(appRoot, 'src/features/common/selectors/s1.js'))).to.be.true;
+// exec('npm test features/common/selectors/s1.test.js', { cwd: appRoot });
 
-exec('rekit rm selector common/s1', { cwd: appRoot });
-expect(shell.test('-e', path.join(appRoot, 'src/features/common/selectors/s1.js'))).to.be.false;
+// exec('rekit rm selector common/s1', { cwd: appRoot });
+// expect(shell.test('-e', path.join(appRoot, 'src/features/common/selectors/s1.js'))).to.be.false;
 

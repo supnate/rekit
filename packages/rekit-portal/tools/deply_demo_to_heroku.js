1c04e8830fbc0c1856c6ab88bbc05841e1616286
Nate Wang <supnate@gmail.com>
2018-02-01 00:32:43 +0800

    Add instruction about how to deply to heroku.

---
+++ packages/rekit-portal/tools/deply_demo_to_heroku.js
@@ -0,0 +1,7 @@
+// 1. heroku login
+// 2. git clone https://git.heroku.com/rekit-portal.git heroku-rekit-portal
+// 3. Delete all files except .git folder
+// 4. copy files from rekit/packages/rekit-portal to heroku-rekit-portal folder.
+// 5. Commit files
+// 6. git push origin master
+ 

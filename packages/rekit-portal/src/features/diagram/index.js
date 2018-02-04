d4c6735e618162488327815ee7ff415671d4f2ec
Nate Wang <supnate@gmail.com>
2018-02-04 18:36:34 +0800

    Add feature diagram page.

--- packages/rekit-portal/src/features/diagram/index.js
+++ packages/rekit-portal/src/features/diagram/index.js
@@ -2,3 +2,4 @@ export { default as ElementDiagram } from './ElementDiagram';
 export { default as FeatureDiagram } from './FeatureDiagram';
 export { default as OverviewDiagram } from './OverviewDiagram';
 export { default as OverviewChordDiagram } from './OverviewChordDiagram';
+export { default as FeatureDiagramPage } from './FeatureDiagramPage';

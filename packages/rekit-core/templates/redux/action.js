be315e2aa91af372028c6addbdc6f75e442b2cdf
Nate Wang <supnate@gmail.com>
2018-02-05 14:38:45 +0800

    Add comment in action template.

--- packages/rekit-core/templates/redux/action.js
+++ packages/rekit-core/templates/redux/action.js
@@ -1,3 +1,7 @@
+// Rekit uses a new approach to organizing actions and reducers. That is
+// putting related actions and reducers in one file. See more at:
+// https://medium.com/@nate_wang/a-new-approach-for-managing-redux-actions-91c26ce8b5da
+
 import {
   ${actionType},
 } from './constants';

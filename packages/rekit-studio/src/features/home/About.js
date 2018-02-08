752539029bee86e1e5775684486e48bdb35daaab
Nate Wang <supnate@gmail.com>
2018-02-08 17:55:00 +0800

    Add link in demo dialog.

--- packages/rekit-studio/src/features/home/About.js
+++ packages/rekit-studio/src/features/home/About.js
@@ -76,6 +76,7 @@ export class About extends Component {
         </p>
         <h3>Learn more</h3>
         <ul className="learn-more-list">
+          <li><a href="https://medium.com/p/introducing-rekit-studio-a-real-ide-for-react-and-redux-development-baf0c99cb542" target="_blank">Introducing Rekit Studio: a real IDE for React and Redux development</a></li>
           <li><a href="https://medium.com/@nate_wang/rekit-2-0-next-generation-react-development-ce8bbba51e94" target="_blank">Rekit 2.0 is out with great new features!</a></li>
           <li><a href="http://rekit.js.org" target="_blank">Rekit docs: rekit.js.org</a></li>
           <li><a href="https://medium.com/@nate_wang/feature-oriented-architecture-for-web-applications-2b48e358afb0" target="_blank">Feature oriented web development with React, Redux and React-router</a></li>

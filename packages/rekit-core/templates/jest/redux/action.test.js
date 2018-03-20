9de29839aa5b0ac5fbfc6484f5819dac70f3dc84
Nate Wang <supnate@gmail.com>
2018-03-20 15:19:44 +0800

    Add action test tpl for jest.

--- packages/rekit-core/templates/jest/redux/action.test.js
+++ packages/rekit-core/templates/jest/redux/action.test.js
@@ -1,17 +1,15 @@
-import { expect } from 'chai';
-
 import {
   ${actionType},
-} from 'src/features/${_.kebabCase(feature)}/redux/constants';
+} from '../../../../src/features/${_.kebabCase(feature)}/redux/constants';
 
 import {
   ${_.camelCase(action)},
   reducer,
-} from 'src/features/${_.kebabCase(feature)}/redux/${_.camelCase(action)}';
+} from '../../../../src/features/${_.kebabCase(feature)}/redux/${_.camelCase(action)}';
 
 describe('${_.kebabCase(feature)}/redux/${_.camelCase(action)}', () => {
   it('returns correct action by ${_.camelCase(action)}', () => {
-    expect(${_.camelCase(action)}()).to.have.property('type', ${actionType});
+    expect(${_.camelCase(action)}()).toHaveProperty('type', ${actionType});
   });
 
   it('handles action type ${actionType} correctly', () => {
@@ -20,7 +18,10 @@ describe('${_.kebabCase(feature)}/redux/${_.camelCase(action)}', () => {
       prevState,
       { type: ${actionType} }
     );
-    expect(state).to.not.equal(prevState); // should be immutable
-    expect(state).to.deep.equal(prevState); // TODO: replace this line with real case.
+    // Should be immutable
+    expect(state).not.toBe(prevState);
+
+    // TODO: use real case expected value instead of {}.
+    expect(state).toEqual({});
   });
 });

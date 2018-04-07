b2a6ef216f1a616bc02dd5a9bc3f983a76dd9f3f
Nate Wang <supnate@gmail.com>
2018-04-07 19:30:12 +0800

    Code clean.

--- packages/rekit-studio/src/features/home/route.js
+++ packages/rekit-studio/src/features/home/route.js
@@ -11,8 +11,6 @@ export default {
   name: 'Home',
   childRoutes: [
     { name: 'Home page', component: HomePage, isIndex: true },
-    // { path: '/element', name: 'Element page', component: ElementPage },
-    // { path: '/element/:type', name: 'Element page', component: ElementPage },
     { path: '/welcome', name: 'Welcome page', component: WelcomePage },
     { path: '/all-routes', name: 'All Routes', component: AllRoutesPage },
     { path: '/element/:file/:type?', name: 'Element page', component: ElementPage },

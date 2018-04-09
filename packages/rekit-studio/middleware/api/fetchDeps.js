37f3d44f10e3dda0251712bf9d408937ca90627e
Nate Wang <supnate@gmail.com>
2018-04-10 00:02:42 +0800

    Fetch deps versions.

--- packages/rekit-studio/middleware/api/fetchDeps.js
+++ packages/rekit-studio/middleware/api/fetchDeps.js
@@ -1,15 +1,26 @@
 'use strict';
 
 const rekitCore = require('rekit-core');
+const _ = require('lodash');
 
 const utils = rekitCore.utils;
 
 function fetchDeps() {
   const prjRoot = utils.getProjectRoot();
   const prjPkgJson = require(utils.joinPath(prjRoot, 'package.json')); // eslint-disable-line
+  const allDeps = Object.assign({}, prjPkgJson.dependencies, prjPkgJson.devDependencies);
+  Object.keys(allDeps).forEach(key => {
+    allDeps[key] = {
+      requiredVersion: allDeps[key],
+      installedVersion: require(`${key}/package.json`).version, // eslint-disable-line
+      lastestVersion: 'TODO',
+    };
+  });
+
   return {
-    deps: prjPkgJson.depencencies || {},
-    devDeps: prjPkgJson.devDependencies || {},
+    deps: Object.keys(prjPkgJson.dependencies || {}),
+    devDeps: Object.keys(prjPkgJson.devDependencies || {}),
+    allDeps,
   };
 }
 

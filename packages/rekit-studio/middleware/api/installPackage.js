bd023de5534926d4da94c05d0be6d0e5d2a55efe
Nate Wang <supnate@gmail.com>
2018-04-12 21:18:24 +0800

    Install package via api.

---
+++ packages/rekit-studio/middleware/api/installPackage.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const rekitCore = require('rekit-core');
+const spawn = require('child_process').spawn;
+
+function installPackage(io, name, type, version) {
+  const prjRoot = rekitCore.utils.getProjectRoot();
+  return new Promise((resolve) => {
+    const isCra = fs.existsSync(path.join(prjRoot, 'scripts/build.js'));
+
+    const args = [];
+    if (isCra) args.push(`${prjRoot}/scripts/build.js`, '--colors');
+    else args.push(`${prjRoot}/tools/build.js`);
+    const child = spawn('node',
+      args,
+      {
+        stdio: 'pipe',
+        cwd: prjRoot
+      }
+    );
+    child.stdout.pipe(process.stdout);
+    const handleOutput = (data) => {
+      // collect the data
+      const text = data.toString('utf8').replace(/ /g, '&nbsp;').split('\n');
+
+      const arr = [];
+      text.forEach(t => arr.push(t));
+      io.emit('output', {
+        type: 'build',
+        output: arr,
+      });
+    };
+    child.stdout.on('data', handleOutput);
+    child.stderr.on('data', handleOutput);
+
+    child.on('close', () => {
+      io.emit('build-finished', {});
+      resolve();
+    });
+  });
+}
+
+module.exports = installPackage;

bb234ef57e7f5c39a1f7fb816167b5c0363bc66c
Nate Wang <supnate@gmail.com>
2018-04-15 01:26:15 +0800

    Install package using task runner.

--- packages/rekit-studio/middleware/taskRunner.js
+++ packages/rekit-studio/middleware/taskRunner.js
@@ -1,1 +1,42 @@
 // Run async tasks using socket.io to push to client.
+
+'use strict';
+
+const rekitCore = require('rekit-core');
+const spawn = require('child_process').spawn;
+
+function runTask(io, cmd, type, id) {
+  const prjRoot = rekitCore.utils.getProjectRoot();
+  const args = cmd.split(' ');
+  return new Promise((resolve) => {
+    const child = spawn(args[0],
+      args.splice(1),
+      {
+        stdio: 'pipe',
+        cwd: prjRoot
+      }
+    );
+    child.stdout.pipe(process.stdout);
+    const handleOutput = (data) => {
+      // collect the data
+      const text = data.toString('utf8').replace(/ /g, '&nbsp;').split('\n');
+
+      const arr = [];
+      text.forEach(t => arr.push(t));
+      io.emit('output', {
+        type,
+        id,
+        output: arr,
+      });
+    };
+    child.stdout.on('data', handleOutput);
+    child.stderr.on('data', handleOutput);
+
+    child.on('close', () => {
+      io.emit(`${type}-finished`, {});
+      resolve();
+    });
+  });
+}
+
+module.exports = runTask;

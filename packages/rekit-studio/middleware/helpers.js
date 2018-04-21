8976592b08248551c4723270635226d4385f1f7e
Nate Wang <supnate@gmail.com>
2018-04-21 18:06:59 +0800

    UI to update package.

--- packages/rekit-studio/middleware/helpers.js
+++ packages/rekit-studio/middleware/helpers.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
+const path = require('path');
 const rekitCore = require('rekit-core');
 
+const utils = rekitCore.utils;
+let usingYarn = false;
+let prjRoot = utils.getProjectRoot();
+let lastDir;
+while (prjRoot && prjRoot !== lastDir && fs.existsSync(prjRoot)) {
+  if (fs.existsSync(utils.joinPath(prjRoot, 'yarn.lock'))) {
+    usingYarn = true;
+    break;
+  }
+  lastDir = prjRoot;
+  prjRoot = path.join(prjRoot, '..');
+}
 module.exports = {
-  forceRequire(path) {
+  forceRequire(modulePath) {
     // Avoid cache for require.
-    delete require.cache[path];
-    return require(path); // eslint-disable-line
+    delete require.cache[modulePath];
+    return require(modulePath); // eslint-disable-line
   },
 
-  usingYarn: fs.existsSync(rekitCore.utils.joinPath(rekitCore.getProjectRoot(), 'yarn.lock')),
+  usingYarn,
 };

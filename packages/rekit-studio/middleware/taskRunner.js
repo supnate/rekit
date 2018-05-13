db427b6fe8e8d8f812884f346dacd6471566eda8
Nate Wang <supnate@gmail.com>
2018-05-13 14:56:42 +0800

    Use _.throttle for socket output.

--- packages/rekit-studio/middleware/taskRunner.js
+++ packages/rekit-studio/middleware/taskRunner.js
@@ -4,11 +4,16 @@
 
 const rekitCore = require('rekit-core');
 const spawn = require('child_process').spawn;
+const _ = require('lodash');
 
 function runTask(io, cmd, type, params) {
   console.log('running task: ', cmd);
   const prjRoot = rekitCore.utils.getProjectRoot();
   const args = cmd.split(' ');
+
+  // const doOutput = (data) => {
+
+  // }
   return new Promise((resolve) => {
     const child = spawn(args[0],
       args.splice(1),
@@ -18,23 +23,35 @@ function runTask(io, cmd, type, params) {
       }
     );
     child.stdout.pipe(process.stdout);
+
+    const arr = [];
+    // Emit output at most every second
+    const emitOutput = _.throttle(() => {
+      if (arr.length > 0) io.emit('output', {
+        type,
+        id: params.id,
+        params,
+        output: arr,
+      });
+      arr.length = 0;
+    }, 1000);
     const handleOutput = (data) => {
       // collect the data
       const text = data.toString('utf8').replace(/ /g, '&nbsp;').split('\n');
+      arr.push.apply(arr, text);
+      emitOutput();
+    };
+    child.stdout.on('data', handleOutput);
+    child.stderr.on('data', handleOutput);
 
-      const arr = [];
-      text.forEach(t => arr.push(t));
+    child.on('close', () => {
       io.emit('output', {
         type,
         id: params.id,
         params,
         output: arr,
       });
-    };
-    child.stdout.on('data', handleOutput);
-    child.stderr.on('data', handleOutput);
-
-    child.on('close', () => {
+      arr.length = 0;
       io.emit('task-finished', { type, params });
       resolve();
     });

12c69787834bd79e71e1889177e658bae862e084
Marcelo Junior <marceloadsj1@gmail.com>
2018-07-01 13:59:31 -0300

    Importing the new env vars component in entries and routes

--- packages/rekit-studio/src/features/config/index.js
+++ packages/rekit-studio/src/features/config/index.js
@@ -1,3 +1,5 @@
 export { default as WebpackManager } from './WebpackManager';
 export { default as DepsManager } from './DepsManager';
 export { default as DepsList } from './DepsList';
+export { default as EnvVarsManager } from './EnvVarsManager';
+export { default as EnvVarsList } from './EnvVarsList';

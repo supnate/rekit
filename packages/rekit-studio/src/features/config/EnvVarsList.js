a58513fd4d86465ce9fafdd7ea48af4c49d44cec
Marcelo Junior <marceloadsj1@gmail.com>
2018-07-01 13:58:43 -0300

    Creating the EnvVarsManager and EnvVarsList components/styles/tests

---
+++ packages/rekit-studio/src/features/config/EnvVarsList.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Icon, Table } from 'antd';
+
+export default class EnvVarsList extends Component {
+  static propTypes = {
+    generalEnvVars: PropTypes.array.isRequired,
+    localEnvVars: PropTypes.array.isRequired,
+    env: PropTypes.string.isRequired,
+  };
+
+  getEnv() {
+    const { env } = this.props;
+    return `${env[0].toUpperCase()}${env.substr(1)}`;
+  }
+
+  getColumns() {
+    return [
+      {
+        dataIndex: 'name',
+        title: 'Name',
+      },
+      {
+        dataIndex: 'value',
+        title: 'Value',
+      },
+    ];
+  }
+
+  render() {
+    return (
+      <div className="config-env-vars-list">
+        <div className="toolbar no-top-margin">
+          <h4>{this.getEnv()}</h4>
+
+          <Button type="primary" size="small" onClick={this.handleAddEnvVar}>
+            Add
+          </Button>
+        </div>
+
+        <h5>General</h5>
+        <Table
+          columns={this.getColumns()}
+          dataSource={this.props.generalEnvVars}
+          size="small"
+          pagination={false}
+          rowKey="name"
+        />
+
+        <br />
+
+        <h5>Local</h5>
+        <Table
+          columns={this.getColumns()}
+          dataSource={this.props.localEnvVars}
+          size="small"
+          pagination={false}
+          rowKey="name"
+        />
+      </div>
+    );
+  }
+}

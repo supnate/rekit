0c77c018c09673acee68b1ce5e67acd1f49ebc23
Nate Wang <supnate@gmail.com>
2018-08-30 14:23:56 +0800

    Scroll to bottom when new output.

--- packages/rekit-studio/src/features/home/OutputView.js
+++ packages/rekit-studio/src/features/home/OutputView.js
@@ -9,11 +9,24 @@ export class OutputView extends Component {
     output: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired,
   };
+  getSnapshotBeforeUpdate() {
+    // why not called?
+    const n = this.scrollNode;
+    console.log('snapshot: ', n.scrollHeight, n.scrollTop, n.offsetHeight);
+    return n.scrollHeight - n.scrollTop < n.offsetHeight * 1.8;
+  }
+  componentDidUpdate(prevProps, prevState, needScrollBottom) {
+    const n = this.scrollNode;
+    n.scrollTop = n.scrollHeight - n.offsetHeight;
+
+  }
+
+  assignRef = node => this.scrollNode = node;
 
   render() {
     const { output } = this.props;
     return (
-      <div className="home-output-view">
+      <div className="home-output-view" ref={this.assignRef}>
         <ul>
           {output.length === 0 && <li key="empty">No output.</li>}
           {output.map((html, i) => <li key={i} dangerouslySetInnerHTML={{ __html: html }} />)}

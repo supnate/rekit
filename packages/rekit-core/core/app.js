c8a7b9700589e9b48e447947610a6a495ff41ae4
Nate Wang <supnate@gmail.com>
2018-09-14 17:58:10 +0800

    Ensure project data loaded.

--- packages/rekit-core/core/app.js
+++ packages/rekit-core/core/app.js
@@ -9,12 +9,12 @@ const plugin = require('./plugin');
 
 function getProjectData() {
   const plugins = plugin.getPlugins('app.getProjectData');
+  const prjData = {};
   if (plugins.length) {
-    const prjData = {};
     plugins.forEach(p => Object.assign(prjData, p.app.getProjectData()));
-    return prjData;
   }
-  return readDir(paths.map('src'));
+  if (!prjData.elements || !prjData.elementById) Object.assign(prjData, readDir(paths.map('src')));
+  return prjData;
 }
 
 function sortElements(elements, elementById) {

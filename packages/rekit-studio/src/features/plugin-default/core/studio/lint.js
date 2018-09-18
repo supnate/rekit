4d2958285038791d2f6ca70e109190090e0bfea4
Nate Wang <supnate@gmail.com>
2018-09-19 00:18:46 +0800

    Use local eslint in the project.

--- packages/rekit-studio/src/features/plugin-default/core/studio/lint.js
+++ packages/rekit-studio/src/features/plugin-default/core/studio/lint.js
@@ -1,11 +1,15 @@
 const path = require('path');
-const CLIEngine = require('eslint').CLIEngine;
+const importFrom = require('import-from');
+
 
 function lint(req, res) {
   const code = req.body.content;
   const file = rekit.core.paths.map(req.body.file);
+  const cwd = path.dirname(file);
+  const CLIEngine = importFrom(cwd, 'eslint').CLIEngine;
+
   const cli = new CLIEngine({
-    cwd: path.dirname(file),
+    cwd,
   });
   try {
     const messages = cli.executeOnText(code, file).results[0].messages;

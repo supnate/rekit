408a346d06420bd40f019f21816cae1f35b6c737
Nate Wang <supnate@gmail.com>
2018-10-14 00:43:35 +0800

    Plugin build in progress.

---
+++ packages/rekit-studio/src/features/plugin-cra/Aa.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Aa extends Component {
+  static propTypes = {
+
+  };
+
+  render() {
+    return (
+      <div className="plugin-cra-aa">
+        Component content: plugin-cra/Aa
+      </div>
+    );
+  }
+}
--- packages/rekit-studio/src/features/plugin-cra/index.js
+++ packages/rekit-studio/src/features/plugin-cra/index.js
@@ -2,3 +2,4 @@ export { default as DefaultPage } from './DefaultPage';
 export { default as ElementDiagram } from './ElementDiagram';
 export { default as app } from './app';
 export { default as RouteRulesView } from './RouteRulesView';
+export { default as Aa } from './Aa';

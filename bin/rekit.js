f18a52b82728a08ad446d809118946b74de8bb75
Nate Wang <supnate@gmail.com>
2019-05-25 16:01:50 +0800

    Remove args of rekit-react plugin.

--- bin/rekit.js
+++ bin/rekit.js
@@ -94,24 +94,6 @@ addCmd.addArgument('name', {
     "The element name to add, in format of <feature>/<name>, e.g.: 'rekit add component user/list-view'. <name> is unnecessary if add a feature.",
 });
 
-// TODO: move to rekit-react plugin
-addCmd.addArgument(['--connect', '-c'], {
-  help: 'Whether to connect to the Redux store. Only used for component.',
-  action: 'storeTrue',
-});
-
-// TODO: move to rekit-react plugin
-addCmd.addArgument(['--url-path', '-u'], {
-  help: 'The url path added to react router config. Only used for page/component.',
-  dest: 'urlPath',
-});
-
-// TODO: move to rekit-react plugin
-addCmd.addArgument(['--async', '-a'], {
-  help: 'Whether the action is async using redux-thunk.',
-  action: 'storeTrue',
-});
-
 // Remove sub-command
 const rmCmd = subparsers.addParser('remove', {
   aliases: ['rm'],
